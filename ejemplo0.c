47ba43db30d85577f4e7e059c6d30f561657711a
diegostrng <157607003+diegostrng@users.noreply.github.com>
2024-07-24 07:57:53 -0500

    Sistemas Operativos - Ejemplo 1
    Código básico para correr en Linux a través de GCC

---
+++ ejemplo0.c
@@ -0,0 +1,49 @@
+/******************************************
+*                                          *
+* Autor: JD                                *
+* Materia: Sistemas Operativos             *
+* Fecha: 18-07-24                          *
+* Tema: Introducción al lenguaje C y los   *
+*       entornos                           *
+*   - Entornos en la web (Cloud/Nube)      *
+*   - Comandos Principales de Linux        *
+*   - Programa 0: Hola Mundo               *
+*                                          *
+*******************************************/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("\n");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\tHola Mundo, Soy Juan Diego\n");
+    printf("\t\tEstoy estudiando Ingenieria de Sistemas en la Universidad Javeriana\n");
+    printf("\t\tVoy en mi Cuarto Semestre, con un promedio de 4,06\n");
+    printf("\t\tEstoy viendo Sistemas Operativos, Estructuras de Datos, Probabilidad y Estadistica Inferencial, etc...\n");
+    printf("\t\tMe encanta mucho la musica, y soy un aficionado a la computacion.\n");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("⢀⡴⠑⡄⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠸⡇⠀⠿⡀⠀⠀⠀⣀⡴⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠑⢄⣠⠾⠁⣀⣄⡈⠙⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⢀⡀⠁⠀⠀⠈⠙⠛⠂⠈⣿⣿⣿⣿⣿⠿⡿⢿⣆⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⢀⡾⣁⣀⠀⠴⠂⠙⣗⡀⠀⢻⣿⣿⠭⢤⣴⣦⣤⣹⠀⠀⠀⢀⢴⣶⣆\n");
+    printf("⠀⠀⢀⣾⣿⣿⣿⣷⣮⣽⣾⣿⣥⣴⣿⣿⡿⢂⠔⢚⡿⢿⣿⣦⣴⣾⠁⠸⣼⡿\n");
+    printf("⠀⢀⡞⠁⠙⠻⠿⠟⠉⠀⠛⢹⣿⣿⣿⣿⣿⣌⢤⣼⣿⣾⣿⡟⠉⠀⠀⠀⠀⠀\n");
+    printf("⠀⣾⣷⣶⠇⠀⠀⣤⣄⣀⡀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠉⠈⠉⠀⠀⢦⡈⢻⣿⣿⣿⣶⣶⣶⣶⣤⣽⡹⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠉⠲⣽⡻⢿⣿⣿⣿⣿⣿⣿⣷⣜⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣶⣮⣭⣽⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⣀⣀⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⠿⠿⠿⠛⠉\n");
+    printf("\n");
+    printf("\n");
+
+    return 0;
+}
